686a5a352fe83e5efbf3b8c78964841575a80706
fernandovtorres <chereibos@gmail.com>
2024-06-12 19:22:12 -0300

    eu gosto do rudinei

--- main.c
+++ main.c
@@ -644,7 +644,7 @@ void printVooFechado(reserva **galera, int reservasTotais){
     fread(&valorFinal, sizeof(float), 1, fs2);
     fclose(fs2);
     printf("Voo Fechado!\n\n");
-    if(RegistroExiste()){
+    if(reservasTotais){
         for(int i = 0; i < reservasTotais; i++){
             printf("%s\n", (*galera)[i].cpf);
             printf("%s", (*galera)[i].nome);
@@ -690,7 +690,7 @@ int main(void) {
             fecharVoo(valortotal, reservasTotais, &galera);
         }
         else {
-            continue;
+            continue;   
         }
     }
     return 0;
